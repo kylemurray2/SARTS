c320a591e11600dbf1753d5624b5248e9e6912a7
Kyle Dennis Murray <kylemurray2@gmail.com>
2023-09-05 16:19:35 -1000

    update

--- docs/installNotes.sh
+++ docs/installNotes.sh
@@ -13,7 +13,10 @@ rm Mambaforge-Linux-x86_64.sh
 # Git SARTS. This has a requirements file we'll use 
 git clone https://github.com/kylemurray2/SARTS.git
 # Make the conda env that will work for isce, fringe, SARTS, and mintpy. Takes awhile..go get a snack
-conda env create -f $softwareDir/SARTS/docs/isce_fringe.yml
+mamba update -n base -c conda-forge mamba
+#conda env create -f $softwareDir/SARTS/docs/requirements.yml
+# use the cloud version if you don't want mdx and spyder
+mamba env create -f $softwareDir/SARTS/docs/requirements_cloud.yml
 
 # Install ISCE-2
 mkdir src
@@ -23,22 +26,21 @@ cd $softwareDir/src/isce2
 mkdir build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$softwareDir/isce -DPYTHON_MODULE_DIR=$softwareDir/isce/python -DCMAKE_PREFIX_PATH=${CONDA_PREFIX} -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX=$softwareDir/isce2 -DPYTHON_MODULE_DIR=$softwareDir -DCMAKE_PREFIX_PATH=${CONDA_PREFIX} -DCMAKE_BUILD_TYPE=Release
 make -j 16 # to use multiple threads
 make install
 
-#copy the contrib and applications directory from src to the install dir
-cp -r $softwareDir/src/isce2/contrib $softwareDir/isce/
-cp -r $softwareDir/src/isce2/applications $softwareDir/isce/
+#copy the contrib directory from src to the install dir, or just source to src/isce2
+# cp -r $softwareDir/src/isce2/contrib $softwareDir/isce/
 
 
 # Install Fringe
 mkdir $softwareDir/Fringe
 cd $softwareDir/Fringe
 mkdir install build src
+cd src
 git clone https://github.com/kylemurray2/fringe.git
-cd build
-
+cd ../build
 CXX=${CXX} cmake -DCMAKE_INSTALL_PREFIX=../install ../src/fringe
 make all
 make install
@@ -57,7 +59,7 @@ export PYTHONPATH=$PYTHONPATH:$softwareDir/Fringe/install/bin
 export PYTHONPATH=$PYTHONPATH:$softwareDir/Fringe/install/python
 
 # ISCE
-export ISCE_ROOT=$softwareDir/isce
+export ISCE_ROOT=$softwareDir/isce2
 export ISCE_SRC_ROOT=$softwareDir/src/isce2
 export PATH=$PATH:$ISCE_ROOT:$ISCE_ROOT/bin:$ISCE_ROOT/applications
 export PYTHONPATH=$PYTHONPATH:$ISCE_ROOT
