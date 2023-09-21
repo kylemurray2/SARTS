c2298431d0facd8ba8bc336312259117844bff18
Kyle Dennis Murray <kylemurray2@gmail.com>
2023-09-21 13:05:51 -1000

    update

--- runFringe.py
+++ runFringe.py
@@ -1,150 +1,123 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  9 15:06:09 2023
-
-@author: km
-"""
-
+import os,glob,yaml,argparse
 import numpy as np
-import os
-import glob
 from matplotlib import pyplot as plt
 from osgeo import gdal
-import argparse
-import sequential_PL 
-import tops2vrt
-import nmap
-import adjustMiniStacks
-import ampdispersion
+import tops2vrt,nmap,sequential_PL,adjustMiniStacks,ampdispersion
 from SARTS import config,util
 
-ps = config.getPS()
-inps = argparse.Namespace()
-
-# For tops2vrt
-inps.indir      = ps.mergeddir
-inps.stackdir   = './Fringe/coreg_stack'
-inps.geomdir    = './Fringe/geometry'
-inps.outdir     = './Fringe/slcs'
-inps.bbox       = np.array([ps.cropymin,ps.cropymax,ps.cropxmin,ps.cropxmax])
-inps.geobbox    = None
-
-# For nmap
-inps.inputDS        = './Fringe/coreg_stack/slcs_base.vrt'
-inps.outputDS       = './Fringe/KS2/nmap'
-inps.countDS        = './Fringe/KS2/count'
-
-if os.path.isfile('./merged/geom_reference/waterMask.rdr.full.crop.vrt'):
-    inps.maskDS     ='./merged/geom_reference/waterMask.rdr.full.crop.vrt'
-else:
-    inps.maskDS     = None
-
-inps.linesPerBlock  = 256
-inps.memorySize  = ps.maxMem
-inps.halfWindowX = 11
-inps.halfWindowY = 5
-inps.pValue      = 0.05
-inps.method      = 'KS2'
-inps.noGPU       = 'True'
-
-# For sequential_PL
-inps.inputDir       = ps.slcdir
-inps.weightDS       = inps.outputDS
-inps.outputDir      = './Fringe/Sequential'
-inps.memorySize     = inps.memorySize
-inps.minNeighbors   = 5
-inps.miniStackSize  = 20
-inps.forceprocessing= False
-
-# For adjustMiniStacks
-inps.slcDir = './Fringe/slcs/'
-inps.miniStackDir = './Fringe/Sequential/miniStacks/'
-inps.datumDir = './Fringe/Sequential/Datum_connection/'
-inps.outDir = './Fringe/adjusted_wrapped_DS'
-inps.unwrapped = False
-
-# For ampdispersion
-inps.meanampDS = './Fringe/ampDispersion/mean'
-inps.refBand = 1
-inps.outputAD = './Fringe/ampDispersion/ampdispersion' # outputDS is different for nmap, so define it here.
-ampDispersionThreshold = '0.35' # conservative to bias towards DS over PS
-
-
-inps.nx = str(ps.nx)
-inps.ny = str(ps.ny)
-
-#______________________________________________________________________________
-tops2vrt.main(inps)
-
-nmap.main(inps)
-
-sequential_PL.main(inps)
-ds_SLCS = glob.glob(inps.outDir + '/*slc')
-for fn_slc in ds_SLCS:
-    util.write_xml(fn_slc,inps.nx,inps.ny,1,dataType='CFLOAT',scheme='BIP')
-ds_tcorrs= glob.glob(inps.outDir + '/*bin')
-for fn_slc in ds_tcorrs:
-    util.write_xml(fn_slc,inps.nx,inps.ny,1,dataType='CFLOAT',scheme='BIP')
-ds_SLCS = glob.glob('./Fringe/Sequential/Datum_connection/EVD/*slc')
-for fn_slc in ds_SLCS:
-    util.write_xml(fn_slc,inps.nx,inps.ny,1,dataType='CFLOAT',scheme='BIP')
-
-adjustMiniStacks.main(inps)
-slcFns = glob.glob( inps.outDir + '/*slc')
-for  f in slcFns:
-    util.write_xml(f,inps.nx,inps.ny,1,'CFLOAT','BSQ')
-
-ampdispersion.main(inps)
-util.write_xml(inps.outputAD,inps.nx,inps.ny,1,'FLOAT','BSQ')
-util.write_xml(inps.meanampDS,inps.nx,inps.ny,1,'FLOAT','BSQ')
-#______________________________________________________________________________
-
-# if ps.sensor=='ALOS':
-#     os.system('phase_link.py -i ./Fringe/coreg_stack/slcs_base.vrt -w ./Fringe/KS2/nmap -o ./Fringe/PhaseLink -r 70000 -x 11 -y 5')
-#     ds_SLCS = glob.glob('./Fringe/PhaseLink/*slc')
-#     for fn_slc in ds_SLCS:
-#         util.write_xml(fn_slc,nx,ny,1,dataType='CFLOAT',scheme='BIP')
-
-
-# ds = gdal.Open('./Fringe/KS2/nmap')
-# nmap = ds.GetVirtualMemArray()
-# nmap = np.asarray(nmap,dtype=np.float32)
-# plt.figure();plt.imshow(nmap[7,:,:])
-
-#Output the ps pixels by using a threshold on ampdispersion
-os.system('imageMath.py -e="a<' + ampDispersionThreshold + '" --a=./Fringe/ampDispersion/ampdispersion  -o ./Fringe/ampDispersion/ps_pixels -t byte')
-
-#make tcorrMean.bin_____________________________________________________________
-miniStacks_tcorr_files = glob.glob('./Fringe/Sequential/miniStacks/*/EVD/tcorr.bin')
-chunk_size = 400  # Number of time slices to load at once
-num_chunks = ps.ny // chunk_size
-tcorrsMean = np.zeros((ps.ny, ps.nx),dtype='float32')
-
-# ii=0;start = ii * chunk_size ;end = start+chunk_size + 1
-
-for ii in range(ps.ny):
-    tc = np.zeros((len(miniStacks_tcorr_files),ps.nx))
-    for jj,mtf in enumerate(miniStacks_tcorr_files):
-        ds = gdal.Open(mtf)
-        tc[jj,:] = ds.GetVirtualMemArray()[ii,:]
-    tcorrsMean[ii,:] = np.nanmean(tc,axis=0)
+def cmdLineParser():
+    '''
+    Command line parser.
+    '''
+    parser = argparse.ArgumentParser(description='Run fringe sequential estimator.')
+    parser.add_argument('-t','--tops2vrt', action='store_true', help='Run tops2vrt function.')
+    parser.add_argument('-n','--nmap', action='store_true', help='Run nmap function.')
+    parser.add_argument('-s','--sequential_PL', action='store_true', help='Run sequential_PL function.')
+    parser.add_argument('-a','--adjustMiniStacks', action='store_true', help='Run adjustMiniStacks function.')
+    parser.add_argument('-d','--ampdispersion', action='store_true', help='Run ampdispersion function.')
     
-plt.figure();plt.imshow(tcorrsMean,vmin=0,vmax=1,cmap='magma');plt.show()
-
-# if ps.sensor=='ALOS':
-#     ds = gdal.Open('Fringe/PhaseLink/tcorr.bin')
-#     tcorrsMean = ds.GetVirtualMemArray()
-#     plt.figure();plt.imshow(tcorrsMean,vmin=0,vmax=1,cmap='magma');plt.show()
-
-# Write the average tcorr file 
-fmt = "GTiff"
-driver = gdal.GetDriverByName(fmt)
-[cols, rows] = tcorrsMean.shape
-outDataRaster = driver.Create("./Fringe/tcorrMean.bin", rows, cols, 1, gdal.GDT_Float32)
-bnd = outDataRaster.GetRasterBand(1)
-bnd.WriteArray(tcorrsMean)
-bnd.FlushCache()
+    return parser.parse_args()
 
 
-# Now use the script ifgs.py to make unwrapped ifgs
+def run_sequential_PL(inps):
+    sequential_PL.main(inps)
+    ds_SLCS = glob.glob(inps.outDir + '/*slc')
+    for fn_slc in ds_SLCS:
+        util.write_xml(fn_slc,inps.nx,inps.ny,1,dataType='CFLOAT',scheme='BIP')
+    ds_tcorrs= glob.glob(inps.outDir + '/*bin')
+    for fn_slc in ds_tcorrs:
+        util.write_xml(fn_slc,inps.nx,inps.ny,1,dataType='CFLOAT',scheme='BIP')
+    ds_SLCS = glob.glob('./Fringe/Sequential/Datum_connection/EVD/*slc')
+    for fn_slc in ds_SLCS:
+        util.write_xml(fn_slc,inps.nx,inps.ny,1,dataType='CFLOAT',scheme='BIP')
+
+def run_adjustMiniStacks(inps):
+    adjustMiniStacks.main(inps)
+    slcFns = glob.glob( inps.outDir + '/*slc')
+    for  f in slcFns:
+        util.write_xml(f,inps.nx,inps.ny,1,'CFLOAT','BSQ')
+        
+def run_ampdispersion(inps):
+    ampdispersion.main(inps)
+    util.write_xml(inps.outputAD,inps.nx,inps.ny,1,'FLOAT','BSQ')
+    util.write_xml(inps.meanampDS,inps.nx,inps.ny,1,'FLOAT','BSQ')
+    
+def main(inps):
+    
+    ps = config.getPS()   
+    inps.indir      = ps.mergeddir
+    inps.bbox       = np.array([ps.cropymin,ps.cropymax,ps.cropxmin,ps.cropxmax])
+    inps.memorySize = ps.maxMem
+    inps.inputDir   = ps.slcdir
+    inps.weightDS   = inps.outputDS
+    inps.nx = str(ps.nx)
+    inps.ny = str(ps.ny)
+    
+    if inps.tops2vrt:
+        tops2vrt.main(inps)
+
+    if inps.nmap:
+        nmap.main(inps)
+
+    if inps.sequential_PL:
+        run_sequential_PL(inps)
+        
+    if inps.adjustMiniStacks:
+        run_adjustMiniStacks(inps)
+
+    if inps.ampdispersion:
+        run_ampdispersion(inps)
+        
+        
+    # if ps.sensor=='ALOS':
+    #     os.system('phase_link.py -i ./Fringe/coreg_stack/slcs_base.vrt -w ./Fringe/KS2/nmap -o ./Fringe/PhaseLink -r 70000 -x 11 -y 5')
+    #     ds_SLCS = glob.glob('./Fringe/PhaseLink/*slc')
+    #     for fn_slc in ds_SLCS:
+    #         util.write_xml(fn_slc,nx,ny,1,dataType='CFLOAT',scheme='BIP')
+    
+    #Output the ps pixels by using a threshold on ampdispersion
+    os.system('imageMath.py -e="a<' + int(ps.ampDispersionThreshold) + '" --a=./Fringe/ampDispersion/ampdispersion  -o ./Fringe/ampDispersion/ps_pixels -t byte')
+
+    #make tcorrMean.bin_____________________________________________________________
+    miniStacks_tcorr_files = glob.glob('./Fringe/Sequential/miniStacks/*/EVD/tcorr.bin')
+    tcorrsMean = np.zeros((ps.ny, ps.nx),dtype='float32')
+    # ii=0;start = ii * chunk_size ;end = start+chunk_size + 1
+    
+    for ii in range(ps.ny):
+        tc = np.zeros((len(miniStacks_tcorr_files),ps.nx))
+        for jj,mtf in enumerate(miniStacks_tcorr_files):
+            ds = gdal.Open(mtf)
+            tc[jj,:] = ds.GetVirtualMemArray()[ii,:]
+        tcorrsMean[ii,:] = np.nanmean(tc,axis=0)
+        
+    plt.figure();plt.imshow(tcorrsMean,vmin=0,vmax=1,cmap='magma');plt.show()
+
+    # if ps.sensor=='ALOS':
+    #     ds = gdal.Open('Fringe/PhaseLink/tcorr.bin')
+    #     tcorrsMean = ds.GetVirtualMemArray()
+    #     plt.figure();plt.imshow(tcorrsMean,vmin=0,vmax=1,cmap='magma');plt.show()
+
+    # Write the average tcorr file 
+    fmt = "GTiff"
+    driver = gdal.GetDriverByName(fmt)
+    [cols, rows] = tcorrsMean.shape
+    outDataRaster = driver.Create("./Fringe/tcorrMean.bin", rows, cols, 1, gdal.GDT_Float32)
+    bnd = outDataRaster.GetRasterBand(1)
+    bnd.WriteArray(tcorrsMean)
+    bnd.FlushCache()
+
+
+    # Now use the script ifgs.py to make unwrapped ifgs
+if __name__ == '__main__':
+    '''
+    Main driver.
+    '''
+    # inps = argparse.Namespace()
+    # inps.tops2vrt = True
+    # inps.nmap = True
+    # inps.sequential_PL = True
+    # inps.adjustministacks = True
+    # inps.ampdispersion = True
+
+    inps = cmdLineParser()
+    main(inps)
