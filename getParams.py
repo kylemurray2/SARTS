e9a56d5e5c239543c1b36ff7b1e64dbaa08ce752
Kyle Dennis Murray <kylemurray2@gmail.com>
2023-11-06 14:02:33 -1000

    update

--- getParams.py
+++ getParams.py
@@ -4,10 +4,7 @@ from SARTS import setupStack
 
 sartPath = setupStack.__file__
 sartPath = os.path.dirname(sartPath)
-
-# If you want to ensure the output always ends with a '/'
 paramPath = os.path.join(sartPath, 'docs', 'params_template.yaml')
-
 
 if not os.path.isfile('./params.yaml')
     print('Copying from ' + paramPath)
