66e0ae50c21e16bc0e8032b49876d3ee3a46d57d
Kyle Dennis Murray <kylemurray2@gmail.com>
2024-02-02 15:40:08 -1000

    update

--- prep_mintpy.py
+++ prep_mintpy.py
@@ -5,31 +5,25 @@ Created on Fri Feb  2 14:13:32 2024
 
 @author: km
 """
-import os,shutil
+import os,shutil,argparse
 from SARTS import  config
 from osgeo import gdal
 import numpy as np
 from scipy.ndimage import uniform_filter
 import matplotlib.pyplot as plt
 
-mpDir = 'MintPy'
 
+def cmdLineParser():
+    '''
+    Command line parser.
+    '''
+    parser = argparse.ArgumentParser(
+        description='Crop and downlook geom files. Save parameters',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', '--mpDir', type=str, dest='mpDir', default='MintPy', help='MintPy Director name')
 
-ps = config.getPS()
+    return parser.parse_args() 
 
-
-sartPath = config.__file__
-sartPath = os.path.dirname(sartPath)
-mpPath = os.path.join(sartPath, 'docs', 'mintpy_template.inps')
-
-inps_fn = os.path.join(mpDir,'inps.cfg')
-
-if not os.path.isdir(mpDir):
-    print("Setting up MintPy directory")
-    os.mkdir(mpDir)
-    shutil.copy(mpPath, inps_fn)
-else:
-    print('MintPy directory already exists.')
 
 def update_cfg_variable(cfg_file_path, variable_name, new_value):
     """
@@ -55,24 +49,6 @@ def update_cfg_variable(cfg_file_path, variable_name, new_value):
             file.write(line)
 
 
-
-# Find a box that has the highest coherence
-# Read in a coherence file
-pair = ps.pairs[-1]
-#load the last 16 images
-cor_stack = []
-for pair in ps.pairs[-16:]:
-    ds = gdal.Open(os.path.join(ps.dolphin_work_dir,'interferograms',pair,'filt_lk.cor'))
-    image = ds.GetVirtualMemArray()
-    image = image.copy()
-    image[np.isnan(image)] = 0
-    cor_stack.append(image)
-    
-cor_stack = np.asarray(cor_stack,dtype=np.float32)
-
-avg_cor = np.nanmean(cor_stack,axis=0)
-original_image = avg_cor
-
 def find_highest_avg_box(original_image, box_size, downsample_factor):
     # Downsample the image
     downsampled_image = original_image[::downsample_factor, ::downsample_factor]
@@ -83,38 +59,71 @@ def find_highest_avg_box(original_image, box_size, downsample_factor):
     max_pos = np.unravel_index(np.argmax(local_mean), local_mean.shape)
     # Scale the coordinates back to the original image size
     y1, x1 = max_pos[0] * downsample_factor, max_pos[1] * downsample_factor
-    
     y1 -=(box_size//2)
     x1 -=(box_size//2)
-    
-    
     y2, x2 = y1 + box_size, x1 + box_size
+    
     return y1, y2, x1, x2
 
+def get_avg_cor(nImages,ps):
+    #load the last nImages images
+    cor_stack = []
+    for pair in ps.pairs[-nImages:]:
+        ds = gdal.Open(os.path.join(ps.dolphin_work_dir,'interferograms',pair,'filt_lk.cor'))
+        image = ds.GetVirtualMemArray()
+        image = image.copy()
+        image[np.isnan(image)] = 0
+        cor_stack.append(image)
+        
+    cor_stack = np.asarray(cor_stack,dtype=np.float32)
+    avg_cor = np.nanmean(cor_stack,axis=0)
+    
+    return avg_cor
 
-# plt.figure();plt.imshow(downsampled_image)
-# plt.figure();plt.imshow(local_mean)
-# plt.figure();plt.imshow(downsampled_image)
+
+def main(inps):
+    ps = config.getPS()
+
+    sartPath = config.__file__
+    sartPath = os.path.dirname(sartPath)
+    mpPath = os.path.join(sartPath, 'docs', 'mintpy_template.inps')
+    inps_fn = os.path.join(inps.mpDir,'inps.cfg')
+    
+    if not os.path.isdir(inps.mpDir):
+        print("Setting up MintPy directory")
+        os.mkdir(inps.mpDir)
+        shutil.copy(mpPath, inps_fn)
+    else:
+        print('MintPy directory already exists.')
+        
+    avg_cor = get_avg_cor(16,ps)
+    
+    box_size = 300
+    downsample_factor =4
+    y1, y2, x1, x2 = find_highest_avg_box(avg_cor, box_size, downsample_factor)
+    
+    print(f'The best {box_size}X{box_size} box is: ')
+    print(f'y1:y2, x1:x2 --> {y1}:{y2}, {x1}:{x2}')
+    x = [x1, x1, x2, x2, x1]
+    y = [y1, y2, y2, y1, y1]
+    
+    plt.figure(figsize=(8,8))
+    plt.imshow(avg_cor, cmap='magma')
+    plt.plot(x, y, 'red', linewidth=2)
+    plt.title('coherence')
+    plt.legend(['Best coherence box'])
+    plt.show()
+    
+    # replace the value in inps.cfg
+    variable_name = 'mintpy.network.aoiYX'
+    new_value = f'{y1}:{y2}, {x1}:{x2}' 
+    update_cfg_variable(inps_fn, variable_name, new_value)
 
 
-box_size = 300
-downsample_factor =4
-y1, y2, x1, x2 = find_highest_avg_box(avg_cor, box_size, downsample_factor)
-
-print(f'The best {box_size}X{box_size} box is: ')
-print(f'y1:y2, x1:x2 --> {y1}:{y2}, {x1}:{x2}')
-x = [x1, x1, x2, x2, x1]
-y = [y1, y2, y2, y1, y1]
-
-plt.figure(figsize=(10, 10))
-plt.imshow(avg_cor, cmap='magma')
-plt.plot(x, y, 'red', linewidth=2)
-plt.title('coherence')
-plt.legend(['Best coherence box'])
-plt.show()
-
-# replace the value in inps.cfg
-variable_name = 'mintpy.network.aoiYX'
-new_value = f'{y1}:{y2}, {x1}:{x2}' 
-update_cfg_variable(inps_fn, variable_name, new_value)
-
+if __name__ == '__main__':
+    '''
+    Main driver.
+    '''
+    # inps = argparse.Namespace()
+    inps = cmdLineParser()
+    main(inps)
